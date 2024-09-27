d2ad363bc64e9f44ff26d1ff0ef29d79f35c9515
jeongminseok <jeong010328@gmail.com>
2024-09-27 17:40:06 +0900

    hi

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors()); //htpp, https 프로토콜을 사용하는 서버 간의 통신
 app.use(express.json()); // express 모듈의 json() 메소드를 사용한다.
 
 app.get('/', (req, res) => {
-  res.send('Hello Wolrd https test completed yeah');
+  res.send('Hello Wolrd https test completed');
 });
 
 app.post('/chat', (req, res) => {
